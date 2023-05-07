44a383e0385fca553c811a9728530466581a7d93
Abraham Varguez <abraham@AdminisatorsMBP.attlocal.net>
2023-05-07 17:12:20 -0400

    getting there

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//NAV BAR VISIBLE
+//NAV BAR VISIBLE----------------------------------------------------------------------------  Difficulty: 4
 
 let visbility = false;
 
@@ -16,7 +16,59 @@ document.querySelector(".logo").addEventListener("click", function () {
   }
 });
 
-//MEET OUR TEAM SLIDER
+//PRODUCTS PAGE---------------------------------------------------------------------------- Difficulty: 8
+
+// Code with an ajax request to fetch the data from the json file
+
+//Creating a new xmlhttp-request object
+//"http" vairable holds all the properties of the object
+let http = new XMLHttpRequest();
+
+//to prepare the request you need to use a open method on "https"
+http.open("get", "item.json", true);
+
+//first argument sets the http method
+//second argument calls the file where the data is
+//third arguement is a keyword that sets the request to be asynchronous
+
+//sends the request to the json file
+http.send();
+
+//Now we need to catch the response
+//using the onload eventlistener method
+http.onload = function () {
+  //Need to set the readystate and status properties
+  //using the this to check the objects as inside the conditional
+  if (this.readyState == 4 && this.status == 200) {
+    //if we get a successful response, then we parse the json data to convert it into the json syntax
+    //and put it into a JS array
+    let products = JSON.parse(this.responseText);
+
+    //create a empty variable to add the incoming data
+
+    let output = " ";
+
+    //create  a for of loop  that loops through the products object
+    //need to add a html template to output variable so it can be pasted on the html
+
+    for (let item of products) {
+      output += `
+         
+      <div class="product">
+        <img class="product__img" src="${item.image}" alt="${item.imageDescription}">
+        <p class="product__title">${item.title}</p>
+        <p class="product__description">${item.description}</p>
+        <p class="product__price"> <span>${item.price}</span> </p>
+      </div>
+      `;
+    }
+    // and finally we will need to add the final call to post all the data into the HTML
+
+    document.querySelector(".products").innerHTML = output;
+  }
+};
+
+//MEET OUR TEAM SLIDER---------------------------------------------------------------------------- Difficulty: 6
 
 //Variable that selects all classes labled 'slide' and puts them into an array
 let sliderImages = document.querySelectorAll(".slide");
@@ -75,3 +127,5 @@ arrowRight.addEventListener("click", function () {
 });
 
 startSlide();
+
+//JAVASCRIPT= WHAT GOES AROUND COMES AROUND 😵‍💫
